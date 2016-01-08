e83dae27141a367bd06ef7366b38db4c6ab871dd
Kevin Miller <scotty_terrific@yahoo.com>
2016-01-07 20:54:46 -0600

    Fixed bootstrap

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -18,6 +18,6 @@ class DoctorsController < ApplicationController
 
   private
   def doctor_params
-    params.require(:doctor).permit(:first_name)
+    params.require(:doctor).permit(:first_name, :last_name, :address, :city)
   end
 end
