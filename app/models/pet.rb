bba369c77af19e4c96d16ff493657f0a05682d10
Kevin Miller <scotty_terrific@yahoo.com>
2016-01-08 03:50:21 -0600

    more validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,7 @@
 class Pet < ActiveRecord::Base
   has_one :customer
   has_one :doctor
+
+  # ENSURES PRESENCE
+  validates :name, :type, :breed, :age, :weight, :last_visited_date, presence: true
 end
