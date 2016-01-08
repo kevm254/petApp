714bd4e8dca733f74b3cb701f57d6242dc047e3a
Kevin Miller <scotty_terrific@yahoo.com>
2016-01-08 02:55:54 -0600

    add validations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,14 @@
 class Doctor < ActiveRecord::Base
-  #validates :first_name, :last_name, :address, :city, :state, :zip, :school, :years_in_practice,  presence: true
+  # ENSURES PRESENCE
+  validates :first_name, :last_name, :address, :city, :state, :zip, :school, :years_in_practice,  presence: true
+
+  # ENSURES NUMERICALITY
+  validates :zip, :years_in_practice, numericality: true
+
+  # ENSURES PROPER RANGE
+  validates :first_name, length: { maximum: 17 }
+  validates :last_name, length: { maximum: 17 }
+  validates :zip, length: { is: 5 }
+  validates :years_in_practice, length: { minimum: 1, maximum: 100 }
+
 end
