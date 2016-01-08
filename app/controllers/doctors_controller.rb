afb3a8eb5c6253564459d77668be3d8fc072057c
Kevin Miller <scotty_terrific@yahoo.com>
2016-01-07 20:17:48 -0600

    Added doctors form

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,23 @@
+class DoctorsController < ApplicationController
+  def index
+    @doctors = Doctor.all
+  end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      flash[:success] = 'This was successfully created'
+      redirect_to doctors_url
+    end
+  end
+
+
+  private
+  def doctor_params
+    params.require(:doctor).permit(:first_name)
+  end
+end
